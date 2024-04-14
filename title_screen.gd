155d48f14cee47fcc247656dfa04712a15709b51
Jacob Preston <pres2300@gmail.com>
2024-04-14 15:43:09 -0700

    Exit button

--- title_screen.gd
+++ title_screen.gd
@@ -1,11 +1,14 @@
 extends Control
 
-@onready var start_button = $MarginContainer/VBoxContainer/MarginContainer/Button
+@onready var start_button = $MarginContainer/VBoxContainer/MarginContainer/HBoxContainer/StartButton
 
 signal start
 
 func _ready():
 	start_button.grab_focus()
 
-func _on_button_pressed():
+func _on_start_button_pressed():
 	start.emit()
+
+func _on_exit_button_pressed():
+	get_tree().quit()
